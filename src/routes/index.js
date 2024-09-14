f3c71a49e6b198ee51c3dd565f4242669b5cfa5b
Akshay Bhamare <akshaybhamare10@gmail.com>
2024-09-14 13:59:42 +0530

    fix: routes - added home and product routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,39 @@
 import { createBrowserRouter } from 'react-router-dom'
 import App from '../App'
+import Error from '../components/Error/Error'
+import Cart from '../pages/Cart/Cart'
+import Home from '../pages/Home/Home'
+import Product from '../pages/Product/Product'
+import Wishlist from '../pages/Wishlist/Wishlist'
 import { ROUTES } from './routes'
 
 const router = createBrowserRouter([
 	{
 		path: ROUTES.ROOT,
 		element: <App />,
-		errorElement: <h5>Error Occured!</h5>,
-		children: [],
+		errorElement: <Error />,
+		children: [
+			{
+				element: <Home />,
+				errorElement: <Error />,
+				index: true,
+			},
+			{
+				path: ROUTES.WISHLIST,
+				element: <Wishlist />,
+				errorElement: <Error />,
+			},
+			{
+				path: ROUTES.CART,
+				element: <Cart />,
+				errorElement: <Error />,
+			},
+			{
+				path: `${ROUTES.PRODUCT}/:id`,
+				element: <Product />,
+				errorElement: <Error />,
+			},
+		],
 	},
 ])
 
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,8 @@
 export const ROUTES = {
 	ROOT: '/',
+	HOME: '/home',
 	CART: '/cart',
 	WISHLIST: '/wishlist',
 	PRODUCT_CATEGORY: '/product-category',
-	PRODUCT: '/product/:id',
+	PRODUCT: '/product',
 }
